35e0c6765ee6cf0b7d38b3e3197a5c5f750d81f2
Simon Fernandez <simon.fernandez1@univ-grenoble-alpes.fr>
2022-08-23 16:53:16 +0200

    Do not try to send thing if 404

--- server.py
+++ server.py
@@ -16,9 +16,7 @@ class RSSServer(BaseHTTPRequestHandler):
             self.wfile.write(dblp_rss())
         else:
             self.send_response(404)
-            self.send_header("Content-type", "text/html")
-            self.end_headers()
-            self.wfile.write(bytes("Invalid feed. Only DBLP is available", 'utf-8'))
+
 
 
 if __name__ == "__main__":
